a8a9a37b6bce7e641db8a7c80f9146a0fce0b2d3
Dan Staples <dan.staples@riseup.net>
2013-08-28 15:55:20 -0400

    removed debian packaging cruft

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(serval-crypto):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="serval-crypto" command="/usr/bin/serval-crypto"
--- debian/serval-crypto.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the serval-crypto package
-#
-0 4	* * *	root	[ -x /usr/bin/serval-crypto_maintenance ] && /usr/bin/serval-crypto_maintenance
